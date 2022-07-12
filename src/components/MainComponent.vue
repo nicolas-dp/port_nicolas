1774e1987636ab8a0269c1f3861525f3dae1c72b
Nicolas De Palma <wellcomenicolas@gmail.com>
2022-07-12 10:11:22 +0200

    Add btn chiamami

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -182,7 +182,7 @@
           <div class="col-6">
             <h3><font-awesome-icon icon="fa-solid fa-phone" /> CONTATTI</h3>
           <ul class="list-unstyled">
-            <li> Telefono: <a href="callto://+393923378249" title="Chiama ora "><img src="/images/chiamami.png" alt="Chiama ora" /></a></li>
+            <li> Telefono: <a class="btn btn-primary" href="callto://+393923378249" title="Chiama ora "> Chiama ora</a></li>
             
             <li> Mail: <a href="wellcomenicolas@gmail.com">Invia una mail</a></li>
             <li> Instagram: </li>
